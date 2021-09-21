e456c7dc96b96440ca56588d896f96df23dd1a19
Jasper <973016037@qq.com>
2021-09-21 23:09:27 +1200

    desktop version done

--- frontend/src/components/AboutUs/Main/AssociateServiceProviders/AssociateServiceProviders.jsx
+++ frontend/src/components/AboutUs/Main/AssociateServiceProviders/AssociateServiceProviders.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import partner_logo from '../../../../static/img/partner.png'
 
 const AssociateServiceProviders = () => {
     return (
@@ -9,6 +10,17 @@ const AssociateServiceProviders = () => {
             <div className='right_column'>
                 <div className='paragraph'>
                     To ensure we give our students the best opportunity to reach their goals, we align ourselves with leaders and specialists in their own fields.
+                </div>
+                <div className='flex associate_service_providers_image_container'>
+                    <div className='associate_service_providers_image_item clear_left'>
+                        <img src={partner_logo}/>
+                    </div>
+                    <div className='associate_service_providers_image_item associate_service_providers_image_item2'>
+                        <img src={partner_logo}/>
+                    </div>
+                    <div className='associate_service_providers_image_item clear_right'>
+                        <img src={partner_logo}/>
+                    </div>
                 </div>
             </div>
         </div>
