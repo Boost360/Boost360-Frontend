4e99b8c89f15c6f7968fd22638eb38df2832e67d
Zach <wangziqi0325@gmail.com>
2021-09-25 23:11:19 +1200

    Update Icon

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -2,31 +2,33 @@ import React from "react";
 import NavBar from "../NavBar/NavBar";
 import './HomePage.css';
 import { AiOutlineWechat } from 'react-icons/ai';
-import {RiKakaoTalkLine} from 'react-icons/ri';
+import { RiKakaoTalkLine } from 'react-icons/ri';
 import { FaInstagram } from "react-icons/fa";
-import { IconContext } from "react-icons";
+import { IconContext } from "react-icons/lib";
 
-export default function HomePage(){
-    return(
+export default function HomePage() {
+    return (
         <div className="HomePage">
 
             <div className="HomePagebody">
                 <div className="messageHome">
                     <p><span>B</span>oost your <span>P</span>erformance</p>
-                </div>                
+                </div>
             </div>
 
-            <div className="HomePagelink">       
-                <div className="HomeIcon">
-                    <AiOutlineWechat/>
+            <IconContext.Provider value={{ size: "24px" }}>
+                <div className="HomePagelink">
+                    <div className="HomeIcon">
+                        <AiOutlineWechat />
+                    </div>
+                    <div className="HomeIcon">
+                        <RiKakaoTalkLine />
+                    </div>
+                    <div className="HomeIcon">
+                        <FaInstagram />
+                    </div>
                 </div>
-                <div className="HomeIcon">
-                    <RiKakaoTalkLine/>
-                </div>
-                <div className="HomeIcon">
-                    <FaInstagram/>
-                </div>
-            </div>
+            </IconContext.Provider>
         </div>
     )
 }
