83722fe23601458ea9cc0af10608993707955551
mhan823 <mlunhan97@gmail.com>
2021-10-15 22:00:39 +1300

    keep info of Contact us use input

--- src/components/ContactUs/Input/ContactInput.jsx
+++ src/components/ContactUs/Input/ContactInput.jsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import './ContactInput.css';
 import { useTranslation } from "react-i18next";
+import { useForm } from "react-hook-form";
 
 export default function ContactInput() {
     const { t, i18n } = useTranslation();
+    const { register, handleSubmit} = useForm();
+    const [contactDetail, setContactDetail] = useState("");
+    const onSubmit = (data) => setContactDetail(JSON.stringify(data));
+
     return(
         <div className="ContactInput-section">
             <div className="ContactInput-info">
                 <p>{t('contact.info')}</p>
             </div>
             <div className="ContactInput">
-                <input placeholder={t('contact.name')}></input>
-                <input placeholder={t('contact.email')}></input>
-                <textarea placeholder={t('contact.msg')}></textarea>
-                <button>{t('contact.send')}</button>
+                <input {...register("Name")} placeholder={t('contact.name')}></input>
+                <input {...register("Email")} placeholder={t('contact.email')}></input>
+                <textarea {...register("Message")} placeholder={t('contact.msg')}></textarea>
+                
+                <button onClick={handleSubmit(onSubmit)}>{t('contact.send')}</button>
+                {/* in this function, it will handle the submit that I made onClick={handleSubmit(onSubmit)} with handleSubmit and onSubmit */}
+
             </div>
         </div>
     )
