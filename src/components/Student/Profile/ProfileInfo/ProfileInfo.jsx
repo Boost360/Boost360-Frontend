1c1bff6d3e8f8c57c9e75fb9dc9050a8627bd428
mhan823 <mlunhan97@gmail.com>
2021-10-16 15:06:32 +1300

    select box done

--- src/components/Student/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Student/Profile/ProfileInfo/ProfileInfo.jsx
@@ -12,26 +12,58 @@ export default function ProfileInfo() {
     return(
         <div className="ProfileInfo">
             <div className="ProfileInputSection">
-                <p className="ProfileMustInput">*</p>
-                <input className="ProfileInput" placeholder="Address"></input>
+                <p className="ProfileMustInput"> </p>
+                <select className="ProfileSelect" {...register("Ethnicity")}>
+                    <option value="" disabled selected>Ethnicity</option>
+                    <option value="human">Human</option>
+                    <option value="alien">Alien</option>
+                    <option value="other">other</option>
+                </select>
             </div>
+            
             <div className="ProfileInputSection">
                 <p className="ProfileMustInput">*</p>
-                <input className="ProfileInput" placeholder="Mobile"></input>
+                <input {...register("Address")} className="ProfileInput" placeholder="Address"></input>
             </div>
             <div className="ProfileInputSection">
                 <p className="ProfileMustInput">*</p>
-                <input className="ProfileInput" placeholder="Email"></input>
-            </div>
-            <div className="ProfileInputSection">
-                <input className="ProfileInput" placeholder="Medical condition"></input>
+                <input {...register("Mobile")} className="ProfileInput" placeholder="Mobile"></input>
             </div>
             <div className="ProfileInputSection">
                 <p className="ProfileMustInput">*</p>
-                <input className="ProfileInput" placeholder="Golf club"></input>
+                <input {...register("Email")} className="ProfileInput" placeholder="Email"></input>
             </div>
             <div className="ProfileInputSection">
-                <input className="ProfileInput" placeholder="School"></input>
+                <p className="ProfileMustInput"> </p>
+                <input {...register("MedicalCondition")} className="ProfileInput" placeholder="Medical condition"></input>
+            </div>
+            <div className="ProfileInputSection">
+                <p className="ProfileMustInput">*</p>
+                <input {...register("GolfClub")} className="ProfileInput" placeholder="Golf club"></input>
+            </div>
+            <div className="ProfileInputSection">
+                <p className="ProfileMustInput">*</p>
+                <select className="ProfileSelect" {...register("YearPlay")}>
+                    <option value="" disabled selected>Years playing golf</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="other">other</option>
+                </select>
+            </div>
+            <div className="ProfileInputSection">
+                <p className="ProfileMustInput"> </p>
+                <input {...register("School")} className="ProfileInput" placeholder="School"></input>
+            </div>
+            <div className="ProfileInputSection">
+                <p className="ProfileMustInput"> </p>
+                <select className="ProfileSelect" {...register("SchoolYear")}>
+                    <option value="" disabled selected>School year</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="other">other</option>
+                </select>
             </div>
         </div>
     )
