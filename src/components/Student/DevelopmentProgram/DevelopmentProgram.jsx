fb22426ccbfda77a413403d3655202555fbab307
mhan823 <mlunhan97@gmail.com>
2021-11-05 00:57:52 +1300

    loading skeleton

--- src/components/Student/DevelopmentProgram/DevelopmentProgram.jsx
+++ src/components/Student/DevelopmentProgram/DevelopmentProgram.jsx
@@ -10,6 +10,7 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
 import {devProgramStudent} from '../../../api/dev-program/devProgramStudent';
+import Skeleton from '@mui/material/Skeleton';
 
 
 const DevelopmentProgram = ({user}) => {
@@ -35,39 +36,51 @@ const DevelopmentProgram = ({user}) => {
     }, []);
 
     return (
-        <div className='development_program_container'>
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell align="center">Session</TableCell>
-                            <TableCell align="center">Date</TableCell>
-                            <TableCell align="center">Topic</TableCell>
-                            <TableCell align="center">Learning Reference</TableCell>
-                            <TableCell align="center">Training PDF</TableCell>
-                            <TableCell align="center">Completed</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.map((row, index) => (
-                            <TableRow
-                                key={row.name}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                className={row.completed? 'development_gray_row' : ''}
-                            >
-                                <TableCell component="th" scope="row" align="center">
-                                    {row.session}
-                                </TableCell>
-                                <TableCell align="center">{row.date.substring(0, 10)}</TableCell>
-                                <TableCell align="center">{row.topic}</TableCell>
-                                <TableCell align="center">{row.learningReference}</TableCell>
-                                <TableCell align="center"><a href={row.file} className='development_program_file_icon'><InsertDriveFileIcon/></a></TableCell>
-                                <TableCell align="center">{row.completed?<Checkbox disabled checked /> : <Checkbox  disabled />}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+        <div>
+            {
+                loading?(
+                    <div className='development_program_container'>
+                        <Skeleton variant="rectangular" width={1200} height={800} />
+                    </div>
+
+                ):(
+                    <div className='development_program_container'>
+                        <TableContainer component={Paper}>
+                            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell align="center">Session</TableCell>
+                                        <TableCell align="center">Date</TableCell>
+                                        <TableCell align="center">Topic</TableCell>
+                                        <TableCell align="center">Learning Reference</TableCell>
+                                        <TableCell align="center">Training PDF</TableCell>
+                                        <TableCell align="center">Completed</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {rows.map((row, index) => (
+                                        <TableRow
+                                            key={row.name}
+                                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                            className={row.completed? 'development_gray_row' : ''}
+                                        >
+                                            <TableCell component="th" scope="row" align="center">
+                                                {row.session}
+                                            </TableCell>
+                                            <TableCell align="center">{row.date.substring(0, 10)}</TableCell>
+                                            <TableCell align="center">{row.topic}</TableCell>
+                                            <TableCell align="center">{row.learningReference}</TableCell>
+                                            <TableCell align="center"><a href={row.file} className='development_program_file_icon'><InsertDriveFileIcon/></a></TableCell>
+                                            <TableCell align="center">{row.completed?<Checkbox disabled checked /> : <Checkbox  disabled />}</TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </div>
+                    
+                )
+            }
         </div>
 
     );
