cb076fd6f7014be37f12f7d5074ce7dbc52d9b47
mhan823 <mlunhan97@gmail.com>
2021-11-19 00:35:28 +1300

    partner api

--- src/components/Partner/Partner.jsx
+++ src/components/Partner/Partner.jsx
@@ -2,13 +2,33 @@ import React, {Component} from 'react';
 import './Partner.css';
 import Main from './Main/Main';
 import Navigation from "./Navigation/Navigation";
+import {getPartners} from '../../api/partner/partner'
 
 const Partner = () => {
+
+    const [partners, setPartner] = React.useState([
+        {
+            _id: '',
+            partnerLogo: '',
+            partnerName: '',
+            partnerLink: '',
+            partnerInfo: ''
+        }
+    ]);
+
+    const [loading, setLoading] = React.useState(true)
+
+    React.useEffect(async () => {
+        let partnerData = await getPartners();
+        setPartner(partnerData);
+        setLoading(false);
+    }, []);
+
     return (
         <div className='partner_container'>
-            <Navigation/>
+            <Navigation partner={partners}/>
             <div className='partner_main_container'>
-                <Main/>
+                <Main partner={partners}/>
             </div>
         </div>
     )
