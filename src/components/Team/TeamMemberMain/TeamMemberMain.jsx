91ab50dbb8b8745e411fbe4b2fffae3464ddbdca
mhan823 <mlunhan97@gmail.com>
2021-11-19 15:57:35 +1300

    loading skeleton

--- src/components/Team/TeamMemberMain/TeamMemberMain.jsx
+++ src/components/Team/TeamMemberMain/TeamMemberMain.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import TeamMember from "./TeamMember/TeamMember";
 import {retrieveAllTeams} from "../../../api/team/team"
+import Skeleton from '@mui/material/Skeleton';
+import { margin } from '@mui/system';
 
 const TeamMemberMain = () => {
 
@@ -24,11 +26,37 @@ const TeamMemberMain = () => {
 
     return (
         <div className='TeamPage_main_container'>
-            {user.map((item) => (
-                <div className='TeamPage_single_member_container'>
-                <TeamMember user={item}/>
-            </div>
-            ))}
+            
+            {
+                loading?(
+                    <div className="team_loading">
+                        <div className="team_loading_avatar">
+                            <Skeleton variant="circular" height={200} width={200} />
+                            <Skeleton variant="rectangular" height={80} width={200} style={{marginTop:'50px'}}/>
+                        </div>
+                        <div className="team_loading_avatar">
+                            <Skeleton variant="circular" height={200} width={200} />
+                            <Skeleton variant="rectangular" height={80} width={200} style={{marginTop:'50px'}}/>
+                        </div>
+                        <div className="team_loading_avatar">
+                            <Skeleton variant="circular" height={200} width={200} />
+                            <Skeleton variant="rectangular" height={80} width={200} style={{marginTop:'50px'}}/>
+                        </div>
+                        <div className="team_loading_avatar">
+                            <Skeleton variant="circular" height={200} width={200} />
+                            <Skeleton variant="rectangular" height={80} width={200} style={{marginTop:'50px'}}/>
+                        </div>
+                    </div>
+                ):(
+                    <div>
+                        {user.map((item) => (
+                            <div className='TeamPage_single_member_container'>
+                            <TeamMember user={item}/>
+                        </div>
+                        ))}
+                    </div>
+                )
+            }  
         </div>
     )
 };
