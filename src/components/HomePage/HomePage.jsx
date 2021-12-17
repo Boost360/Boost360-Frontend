77971895e570cd61195917aea31071107a4817e3
Sarah Hao <sarahhao911@gmail.com>
2021-12-17 21:53:59 +1300

    [Home] add background video

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -5,10 +5,15 @@ import { AiOutlineWechat } from 'react-icons/ai';
 import { RiKakaoTalkLine } from 'react-icons/ri';
 import { FaInstagram } from "react-icons/fa";
 import { IconContext } from "react-icons/lib";
+import Background_png from '../../static/img/Home_background.png';
+import Background_webm from '../../static/img/Home_background.webm';
 
 export default function HomePage() {
     return (
         <div className="HomePage">
+            <video poster={Background_png} autoPlay loop playsInline muted>
+                <source src={Background_webm} type="video/webm" />
+            </video>
 
             <div className="HomePagebody">
             </div>
