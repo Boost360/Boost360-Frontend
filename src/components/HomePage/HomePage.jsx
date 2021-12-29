54d51019bfc4acc48c8e259e281811077ad030aa
Sarah Hao <sarahhao911@gmail.com>
2021-12-29 20:29:11 +1300

    [Home] add background video

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,23 +1,22 @@
-import React from "react";
+import React, {useEffect} from "react";
 import NavBar from "../NavBar/NavBar";
 import './HomePage.css';
 import { AiOutlineWechat } from 'react-icons/ai';
 import { RiKakaoTalkLine } from 'react-icons/ri';
 import { FaInstagram } from "react-icons/fa";
 import { IconContext } from "react-icons/lib";
-import Background_png from '../../static/img/Home_background.png';
 import Background_webm from '../../static/img/Home_background.webm';
 
 export default function HomePage() {
+    useEffect(() => {
+        // set background video playback speed before mount
+        document.getElementById('background_video').playbackRate = '1.8';
+        return () => {
+        }
+    }, [])
     return (
         <div className="HomePage">
-            <video poster={Background_png} autoPlay loop playsInline muted>
-                <source src={Background_webm} type="video/webm" />
-            </video>
-
-            <div className="HomePagebody">
-            </div>
-
+            <video id='background_video' src={Background_webm} autoPlay loop playsInline muted></video>
             <IconContext.Provider value={{ size: "24px" }}>
                 <div className="HomePagelink">
                     <div className="HomeIcon">
