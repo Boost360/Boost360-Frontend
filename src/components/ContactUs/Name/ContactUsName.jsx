b208c12d853b915f6e2e28023d8993ebf184b3d1
Sarah Hao <sarahhao911@gmail.com>
2021-12-29 20:27:55 +1300

    [Contact] hook up translation and more
    - hook up translation
    - change background image
    - fix color and layout error

--- src/components/ContactUs/Name/ContactUsName.jsx
+++ src/components/ContactUs/Name/ContactUsName.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import './ContactUsName.css';
+import {useTranslation} from 'react-i18next';
 
 export default function ContactUsName() {
+    const {t} = useTranslation();
     return(
         <div className="ContactName-section">
             <div className="ContactName-name">
                 <div>
-                    Test
+                    {t('contact.name.first-name')}
                 </div>
                 <div>
-                    User
+                    {t('contact.name.last-name')}
                 </div>
             <p className="nameline"></p>
             </div>
