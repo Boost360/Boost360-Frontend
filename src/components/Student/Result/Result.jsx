2d1292e94f27770f597e26217d10c9eb80469831
mhan823 <mlunhan97@gmail.com>
2022-01-24 17:42:53 +1300

    Update Result page API communication

--- src/components/Student/Result/Result.jsx
+++ src/components/Student/Result/Result.jsx
@@ -7,7 +7,7 @@ import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import ResultInner from "./ResultInner/ResultInner";
-
+import {resultStudent} from '../../../api/result/result';
 
 function TabPanel(props) {
     const { children, value, index, ...other } = props;
@@ -46,12 +46,65 @@ function a11yProps(index) {
 
 const Result = ({user}) => {
     const user_id = user._id;
-
     const [value, setValue] = React.useState(0);
+    const [loading, setLoading] = React.useState(true);
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
+
+    //Data sets for different type of data
+    const [notice, setNotice] = React.useState([]);
+    const [swingData, setSwingData] = React.useState([]);
+    const [equipments, setEquipments] = React.useState([]);
+    const [yardages, setYardages] = React.useState([]);
+    const [physicalDevelopment, setPhysicalDevelopment] = React.useState([]);
+    const [progressReports, setProgressReport] = React.useState([]);
+    const [psychologyDevelopment, setPsychologyDevelopment] = React.useState([]);
+
+    React.useEffect(async () => {
+        let resultData = await resultStudent(user_id);
+        let notice = []
+        let swingData = []
+        let equipments = []
+        let yardages = []
+        let physicalDevelopment = []
+        let progressReport = []
+        let psychologyDevelopment = []
+        for (let i=0; i < resultData.data.length; i++){
+            switch (resultData.data[i].type){
+                case 'notice':
+                    notice.push(resultData.data[i])
+                    break
+                case 'swingData':
+                    swingData.push(resultData.data[i])
+                    break
+                case 'equipments':
+                    equipments.push(resultData.data[i])
+                    break
+                case 'yardages':
+                    yardages.push(resultData.data[i])
+                    break
+                case 'physicalDevelopment':
+                    physicalDevelopment.push(resultData.data[i])
+                    break
+                case 'progressReports':
+                    progressReport.push(resultData.data[i])
+                    break
+                case 'psychologyDevelopment':
+                    psychologyDevelopment.push(resultData.data[i])
+                    break
+            }
+        }
+        setNotice(notice)
+        setSwingData(swingData)
+        setEquipments(equipments)
+        setYardages(yardages)
+        setPhysicalDevelopment(physicalDevelopment)
+        setProgressReport(progressReport)
+        setPsychologyDevelopment(psychologyDevelopment)
+        setLoading(false);
+    }, []);
 
     return (
         <Box className='result_container' sx={{ width: '100%' }}>
@@ -75,25 +128,25 @@ const Result = ({user}) => {
             </Box>
             <div className='result_inner_container'>
                 <TabPanel value={value} index={0}>
-                    <ResultInner resultType={'notice'} user_id={user_id}/>
+                    <ResultInner data={notice} loading={loading}/>
                 </TabPanel>
                 <TabPanel value={value} index={1}>
-                    <ResultInner resultType={'swingData'} user_id={user_id}/>
+                    <ResultInner data={swingData} loading={loading}/>
                 </TabPanel>
                 <TabPanel value={value} index={2}>
-                    <ResultInner resultType={'equipments'} user_id={user_id}/>
+                    <ResultInner data={equipments} loading={loading}/>
                 </TabPanel>
                 <TabPanel value={value} index={3}>
-                    <ResultInner resultType={'yardages'} user_id={user_id}/>
+                    <ResultInner data={yardages} loading={loading}/>
                 </TabPanel>
                 <TabPanel value={value} index={4}>
-                    <ResultInner resultType={'progressReports'} user_id={user_id}/>
+                    <ResultInner data={progressReports} loading={loading}/>
                 </TabPanel>
                 <TabPanel value={value} index={5}>
-                    <ResultInner resultType={'physicalDevelopment'} user_id={user_id}/>
+                    <ResultInner data={physicalDevelopment} loading={loading}/>
                 </TabPanel>
                 <TabPanel value={value} index={6}>
-                    <ResultInner resultType={'psychologyDevelopment'} user_id={user_id}/>
+                    <ResultInner data={psychologyDevelopment} loading={loading}/>
                 </TabPanel>
             </div>
 
