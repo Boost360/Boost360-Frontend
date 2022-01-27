6dc4717996fa29fd8f920a8b67de2724a1117884
Sarah Hao <sarahhao911@gmail.com>
2022-01-27 23:22:41 +1300

    [Contact] conditional wechat modal & Add linkedin
    - Display two wechat QR code depends on language selected.
    - Add linkedin account

--- src/components/WechatModal/WechatModal.jsx
+++ src/components/WechatModal/WechatModal.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import wechat from '../../static/img/wechat.jpg'
+import wechat_en from '../../static/img/wechat_en.jpg'
+import wechat_ch from '../../static/img/wechat_ch.jpg'
 
-function WechatModal({ handleClose }) {
+function WechatModal({ handleClose, chinese=false }) {
     return (
         <div onClick={handleClose} style={{ width: '100vw', height: '100vh', position: 'fixed', top: '0px', left: '0px', display: 'flex', justifyContent: 'center', alignItems: 'center', background: 'rgba(0,0,0,0.5)' }}>
-            <img style={{ height: '50vh', padding: '50px' }} src={wechat} alt="wechat QR code" />
+            <img style={{ height: '50vh', padding: '50px' }} src={chinese ? wechat_ch : wechat_en} alt="wechat QR code" />
         </div>
     )
 }
